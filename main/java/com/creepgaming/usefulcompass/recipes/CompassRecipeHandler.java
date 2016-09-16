7ff58d8c6ce698014ad5d436d87c440e1b6669c5
CreeperShift <maxhenkes@gmail.com>
2016-09-16 20:34:58 +0200

    Fully implemented Custom Recipe Handler
    Recipe handler currently only accepts items in fixed slots but it's
    working and copies the NBT tag. Will be rewritten later.

--- main/java/com/creepgaming/usefulcompass/recipes/CompassRecipeHandler.java
+++ main/java/com/creepgaming/usefulcompass/recipes/CompassRecipeHandler.java
@@ -1,74 +1,92 @@
 package com.creepgaming.usefulcompass.recipes;
 
-import java.util.List;
-
-import com.creepgaming.usefulcompass.items.ItemCompass;
-import com.google.common.collect.Lists;
+import com.creepgaming.usefulcompass.items.ItemRegister;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 
-public class CompassRecipeHandler implements IRecipe{
+public class CompassRecipeHandler implements IRecipe {
 
-	
 	private ItemStack resultItem;
 
+	/*
+	 * This class is currently very crude and was made as a proof on concept.
+	 * Needs to be rewritten to allow for truly shapeless recipe.
+	 * 
+	 * 
+	 */
 
 	@Override
 	public boolean matches(InventoryCrafting inv, World worldIn) {
-        ItemStack itemstack = null;
-        List<ItemStack> list = Lists.<ItemStack>newArrayList();
 
-        for (int i = 0; i < inv.getSizeInventory(); ++i)
-        {
-            ItemStack itemstack1 = inv.getStackInSlot(i);
+		/*
+		 * Currently takes the first and second slot, checks if those contain
+		 * the right items and then returns true.
+		 */
 
-            if (itemstack1 != null)
-            {
-                if (itemstack1.getItem() instanceof ItemCompass)
-                {
+		if (inv.getStackInSlot(0) != null && inv.getStackInSlot(0).getItem() == ItemRegister.itemCompass
+				&& inv.getStackInSlot(1) != null && inv.getStackInSlot(1).getItem() == ItemRegister.itemCompass
+				&& !inv.getStackInSlot(1).hasTagCompound()) {
+			return true;
+		} else {
+			return false;
+		}
 
-
-                    list.add(itemstack1);
-                }else{
-                	return false;
-                }
-            }
-        }
-
-        return itemstack != null && !list.isEmpty();
 	}
 
 	@Override
 	public ItemStack getCraftingResult(InventoryCrafting inv) {
-        return this.resultItem.copy();
+
+		/*
+		 * Checks for nbt tags, and if so copies the current tag to a new
+		 * itemstack
+		 * 
+		 */
+
+		if (inv.getStackInSlot(0).hasTagCompound() == true) {
+
+			NBTTagCompound tag = inv.getStackInSlot(0).getTagCompound();
+
+			ItemStack returnStack = new ItemStack(ItemRegister.itemCompass, 2);
+
+			returnStack.setTagCompound(tag);
+
+			this.resultItem = returnStack;
+			return returnStack;
+
+		}
+
+		return null;
 	}
 
 	@Override
 	public int getRecipeSize() {
-        return 1;
+		return 9;
 	}
 
 	@Override
 	public ItemStack getRecipeOutput() {
-        return this.resultItem;
+		return this.resultItem;
 	}
 
+	/*
+	 * No idea what this does exactly, needs rewriting, looks complicated. Gotta take a look at it at some point. It works for now so I haven't bothered yet.
+	 * 
+	 */
+	
+	
 	@Override
 	public ItemStack[] getRemainingItems(InventoryCrafting inv) {
-        ItemStack[] aitemstack = new ItemStack[inv.getSizeInventory()];
+		ItemStack[] aitemstack = new ItemStack[inv.getSizeInventory()];
 
-        for (int i = 0; i < aitemstack.length; ++i)
-        {
-            ItemStack itemstack = inv.getStackInSlot(i);
-            aitemstack[i] = net.minecraftforge.common.ForgeHooks.getContainerItem(itemstack);
-        }
+		for (int i = 0; i < aitemstack.length; ++i) {
+			ItemStack itemstack = inv.getStackInSlot(i);
+			aitemstack[i] = net.minecraftforge.common.ForgeHooks.getContainerItem(itemstack);
+		}
 
-        return aitemstack;
+		return aitemstack;
 	}
-	
-	
-	
-	
+
 }
