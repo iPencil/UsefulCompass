985858f3c922f1d9d40972b32e90854a2f784e1f
CreeperShift <maxhenkes@gmail.com>
2016-09-17 07:24:03 +0200

    Implemented Shapeless Recipe Handler

--- main/java/com/creepgaming/usefulcompass/recipes/CompassRecipeHandler.java
+++ main/java/com/creepgaming/usefulcompass/recipes/CompassRecipeHandler.java
@@ -4,7 +4,6 @@ import com.creepgaming.usefulcompass.items.ItemRegister;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 
 public class CompassRecipeHandler implements IRecipe {
@@ -12,8 +11,8 @@ public class CompassRecipeHandler implements IRecipe {
 	private ItemStack resultItem;
 
 	/*
-	 * This class is currently very crude and was made as a proof on concept.
-	 * Needs to be rewritten to allow for truly shapeless recipe.
+	 * Custom recipe handler for the compass Allows copying one compass with NBT
+	 * data to a compass without any.
 	 * 
 	 * 
 	 */
@@ -21,44 +20,69 @@ public class CompassRecipeHandler implements IRecipe {
 	@Override
 	public boolean matches(InventoryCrafting inv, World worldIn) {
 
-		/*
-		 * Currently takes the first and second slot, checks if those contain
-		 * the right items and then returns true.
-		 */
+		int i = 0;
+		ItemStack itemstack = null;
 
-		if (inv.getStackInSlot(0) != null && inv.getStackInSlot(0).getItem() == ItemRegister.itemCompass
-				&& inv.getStackInSlot(1) != null && inv.getStackInSlot(1).getItem() == ItemRegister.itemCompass
-				&& !inv.getStackInSlot(1).hasTagCompound()) {
-			return true;
-		} else {
-			return false;
+		for (int j = 0; j < inv.getSizeInventory(); ++j) {
+			ItemStack itemstack1 = inv.getStackInSlot(j);
+
+			if (itemstack1 != null) {
+				if (itemstack1.getItem() == ItemRegister.itemCompass && itemstack1.hasTagCompound()) {
+					if (itemstack != null) {
+						return false;
+					}
+
+					itemstack = itemstack1;
+				} else {
+					if (itemstack1.getItem() != ItemRegister.itemCompass && !itemstack1.hasTagCompound()) {
+						return false;
+					}
+
+					++i;
+				}
+			}
 		}
+
+		return itemstack != null && i > 0;
 
 	}
 
 	@Override
 	public ItemStack getCraftingResult(InventoryCrafting inv) {
 
-		/*
-		 * Checks for nbt tags, and if so copies the current tag to a new
-		 * itemstack
-		 * 
-		 */
+		int i = 0;
+		ItemStack itemstack = null;
 
-		if (inv.getStackInSlot(0).hasTagCompound() == true) {
+		for (int j = 0; j < inv.getSizeInventory(); ++j) {
+			ItemStack itemstack1 = inv.getStackInSlot(j);
 
-			NBTTagCompound tag = inv.getStackInSlot(0).getTagCompound();
+			if (itemstack1 != null) {
+				if (itemstack1.getItem() == ItemRegister.itemCompass && itemstack1.hasTagCompound()) {
+					if (itemstack != null) {
+						return null;
+					}
 
-			ItemStack returnStack = new ItemStack(ItemRegister.itemCompass, 2);
+					itemstack = itemstack1;
+				} else {
+					if (itemstack1.getItem() != ItemRegister.itemCompass && !itemstack1.hasTagCompound()) {
+						return null;
+					}
 
-			returnStack.setTagCompound(tag);
-
-			this.resultItem = returnStack;
-			return returnStack;
-
+					++i;
+				}
+			}
 		}
 
-		return null;
+		if (itemstack != null && i >= 1) {
+			ItemStack itemstack2 = new ItemStack(ItemRegister.itemCompass, i + 1);
+			if (itemstack.hasTagCompound()) {
+				itemstack2.setTagCompound(itemstack.getTagCompound());
+			}
+
+			return itemstack2;
+		} else {
+			return null;
+		}
 	}
 
 	@Override
@@ -71,12 +95,7 @@ public class CompassRecipeHandler implements IRecipe {
 		return this.resultItem;
 	}
 
-	/*
-	 * No idea what this does exactly, needs rewriting, looks complicated. Gotta take a look at it at some point. It works for now so I haven't bothered yet.
-	 * 
-	 */
-	
-	
+
 	@Override
 	public ItemStack[] getRemainingItems(InventoryCrafting inv) {
 		ItemStack[] aitemstack = new ItemStack[inv.getSizeInventory()];
